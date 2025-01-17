89e079626fd300a614f53c85b5466ecaef1b4d6e
ananya <ananyakk71@gmail.com>
2025-01-17 11:50:24 +0530

    helth added

--- BBC Home/js/script.js
+++ BBC Home/js/script.js
@@ -1,5 +1,6 @@
 import { loadHeadContent } from './head.js';
 import { loadNavContent } from './nav.js';
+import { loadMainContent } from './main.js';
 fetch("data.json")
    .then(function (response) {
     return response.json();
@@ -7,6 +8,7 @@ fetch("data.json")
    .then(function (data) {
     loadHeadContent(data);
     loadNavContent(data);
+    loadMainContent(data);
    })
       
    
